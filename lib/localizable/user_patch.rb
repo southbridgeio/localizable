220ae13c051de6254f64e338bddc01c9fd0b8677
cross <cross@vgg.ru>
2016-11-18 14:32:39 +0300

    Patch method name for User.

--- lib/localizable/user_patch.rb
+++ lib/localizable/user_patch.rb
@@ -10,10 +10,23 @@
 module Localizable
   module UserPatch
     def self.included(base)
+      base.send(:include, InstanceMethods)
+
       base.class_eval do
         # Same as typing in the class.
         unloadable # Send unloadable so it will not be unloaded in development.
         safe_attributes 'name_in_english'
+
+        alias_method_chain :name, :localizable
+      end
+    end
+  end
+  module InstanceMethods
+    def name_with_localizable(formatter = nil)
+      if  User.current.language.to_s == 'en' and name_in_english.present?
+        name_in_english
+      else
+        name_without_localizable
       end
     end
   end
